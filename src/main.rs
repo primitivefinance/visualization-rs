d5237c5fc0e904a40085ffa0eb0622af35c00cd4
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-02-15 17:29:53 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod functions;
 fn main() {
 
     // Plot RMM trading curve from a list of prices
-    let name = "rmm_trading_curve".to_string();
-    let K = 10_f64;
+    let name = "RMM Trading Curve".to_string();
+    let k = 10_f64;
     let sigma = 0.6_f64;
     let tau = 3.5_f64;
     let p_0 = 0.0_f64;
     let p_1 = 100.0_f64;
     let n = 100;
     let prices = linspace(p_0, p_1, n).collect::<Vec<f64>>();
-    let (x,y) = functions::rmm_trading_curve(prices, K, sigma, tau);
+    let (x,y) = functions::rmm_trading_curve(prices, k, sigma, tau);
     plot::transparent_plot(x, y, name, false, true);
 
     // Other examples
