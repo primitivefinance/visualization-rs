0271565939c96361218feb5f0c756b6475cc0517
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-29 13:16:07 -0600

    Portfolio Value Plot

--- src/main.rs
+++ src/main.rs
@@ -13,56 +13,34 @@ fn main() {
     let display_mode = DisplayMode::Light;
     let show = true;
 
-    // ------------------ Plotting Plot 5 ------------------ //
-    // Plot RMM liquidity distribution for multiple taus
-    let plot_name = "$\\text{RMM Liquidity Distribution}$".to_string();
-    let strike = 3_f64;
-    let sigma = 0.5_f64;
-    let taus: Vec<f64> = vec![2.0_f64, 1.5_f64, 1.0_f64, 0.5_f64, 0.1_f64];
-    let p_0 = 0.0_f64;
-    let p_1 = 10.0_f64;
-    let n = 1000;
-    let prices = linspace(p_0, p_1, n).collect::<Vec<f64>>();
-    let mut x: Vec<Vec<f64>> = Vec::new();
-    let mut y: Vec<Vec<f64>> = Vec::new();
-    for tau in &taus {
-        x.push(prices.clone());
-        let temp = functions::standard_gaussian_pdf(functions::d_one(prices.clone(), strike, sigma, *tau));
-        let temp = temp.iter().map(|x| x / (sigma * tau.sqrt())).collect::<Vec<f64>>();
-        let mut temp1: Vec<f64> = Vec::new();
-        for (i, y_val) in temp.iter().enumerate() {
-            temp1.push(y_val / prices[i]);
-        }
-        y.push(temp1);
-    };
-    let x_bounds = vec![0_f64, 10_f64];
-    let y_bounds = vec![0_f64, 1_f64];
-    let single_color = false;
-    let colors = vec![
-        (Color::Green, 0, Emphasis::Light, single_color),
-        (Color::Green, 1, Emphasis::Light, single_color),
-        (Color::Green, 2, Emphasis::Light, single_color),
-        (Color::Green, 3, Emphasis::Light, single_color),
-        (Color::Green, plot::MAIN_COLOR_SLOT, Emphasis::Heavy, single_color),
-    ];
-    let legend_names = vec![
-        "$\\tau=2.0$".to_string(),
-        "$\\tau=1.5$".to_string(),
-        "$\\tau=1.0$".to_string(),
-        "$\\tau=0.5$".to_string(),
-        "$\\tau=0.0$".to_string(),
-    ];
+    // Plot 1: Portfolio Value Function
+    let s_values: Vec<f64> = (0..=100).map(|x| x as f64).collect();
+    let v_values: Vec<f64> = s_values.iter().map(|&s| s.sqrt()).collect();
+    let curves = (vec![s_values], vec![v_values]);
+    
+
+    let x_bounds = vec![0.0, 100.0];
+    let y_bounds = vec![0.0, 10.0]; // Update the y-axis upper bound to match the maximum value of V(S) = √S
+    let bounds = (x_bounds, y_bounds);
+    
+    let plot_name = "Portfolio Value Function: V(S) = √S".to_string();
+    
+    let legend_names = Some(vec!["V(S) = √S".to_string()]);
+    let colors = vec![(Color::Green, plot::MAIN_COLOR_SLOT, Emphasis::Heavy, true)];
+
     let labels = Labels {
-        x_label: "$R_x$".to_string(),
-        y_label: "$R_y$".to_string(),
+        x_label: "$S$".to_string(),
+        y_label: "$V(S)$".to_string(),
     };
+
     plot::transparent_plot(
-        (x, y),
-        (x_bounds, y_bounds),
+        (curves),
+        (bounds.0, bounds.1),
         plot_name,
-        Some(legend_names),
+        legend_names,
         colors,
         (transparent, display_mode, show),
         labels,
     );
+
 }
