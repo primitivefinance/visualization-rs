8046c492a03581b50dd2186d7b35ba2c16b5f266
Colin Roberts <colin@primitive.xyz>
2023-04-03 12:25:36 -0600

    steadily adding features

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,53 @@ use plot::{Color, DisplayMode, Emphasis, Labels};
 fn main() {
     // Global Toggle Variables
     let transparent = true;
-    let display_mode = DisplayMode::Light;
+    let display_mode = DisplayMode::Dark;
     let show = true;
 
     // ------------------ Plotting Plot 7 ------------------ //
     let plot_name = "\\text{Leverage Zones}".to_string();
-    let (mut x1, mut y1, mut x2, mut y2) = (vec![], vec![], vec![], vec![]);
-    let x_bounds = vec![0.0, 10.0];
-    let y_bounds = vec![0.0, 10.0];
+    let (mut x, mut y, mut x1, mut y1, mut x2, mut y2) = (vec![], vec![], vec![], vec![], vec![], vec![]);
+    let x_bounds = vec![0.0, 5.0];
+    let y_bounds = vec![0.0, 5.0];
     let t_start = 0.0;
     let t_end = 1.0;
     let number_of_points = 1000;
     let t = linspace(t_start, t_end, number_of_points).collect::<Vec<f64>>(); // Parameter for curves
+    // BUILD CURVES //
+    // y=x^2 curve //
+    x.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
+    y.push(t.iter().map(|t| 25.0*t*t).collect::<Vec<f64>>());
 
-    // y=x line and above (to y=10) //
-    x1.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
-    y1.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
+    // BUILD REGIONS //
+    // y=x line and above (to y=5) //
+    x1.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
+    y1.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
     // y=10 line
-    x2.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
-    y2.push(t.iter().map(|_| 10.0).collect::<Vec<f64>>());
+    x2.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
+    y2.push(t.iter().map(|_| 5.0).collect::<Vec<f64>>());
 
     // y=x line and below (to y=0) //
-    x1.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
-    y1.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
+    x1.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
+    y1.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
     // y=0 line
-    x2.push(t.iter().map(|t| 10.0 * t).collect::<Vec<f64>>());
+    x2.push(t.iter().map(|t| 5.0 * t).collect::<Vec<f64>>());
     y2.push(t.iter().map(|_| 0.0).collect::<Vec<f64>>());
 
     // Get the plot
     let single_color = false;
-    let colors = vec![
+    let curve_colors = vec![
+        (
+            Color::Green,
+            plot::MAIN_COLOR_SLOT,
+            Emphasis::Heavy,
+            single_color,
+        ),
+    ];
+    let curve_legend_names = vec![
+        "V(S)=S^2".to_string(),
+    ];
+
+    let region_colors = vec![
         (
             Color::Purple,
             plot::MAIN_COLOR_SLOT,
@@ -56,7 +73,7 @@ fn main() {
     ];
 
     // TODO: Add legend positioning here
-    let legend_names = vec![
+    let region_legend_names = vec![
         "\\text{Over Levered}".to_string(),
         "\\text{Under Levered}".to_string(),
     ];
@@ -65,12 +82,11 @@ fn main() {
         y_label: "V(S)".to_string(),
     };
 
-    plot::fill_between_curves(
-        ((x1, y1), (x2, y2)),
+    plot::transparent_plot(
+        Some((x,y, curve_colors, Some(curve_legend_names))),
+        Some(((x1, y1), (x2, y2), region_colors, Some(region_legend_names))),
         (x_bounds, y_bounds),
         plot_name,
-        Some(legend_names),
-        colors,
         (transparent, display_mode, show),
         labels,
     );
